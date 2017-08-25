626146592bb5899e723eb5497510311e72cf7952
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-24 19:59:14 -0700

    removed old comment. Added prod channel.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions'),
     admin = require('firebase-admin'),
     http = require('http'),
-WebClient = require('@slack/client').WebClient;
+    WebClient = require('@slack/client').WebClient;
 
 var token = 'xoxb-231024998209-8vXkQdXBtiYKcaj4b3tWhUNF' || '';
 
@@ -11,16 +11,11 @@ exports.sendOrderNotification = functions.database.ref('/Orders/{pushId}').onWri
     postToSlack();
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
 function postToSlack(text) {
 
     var web = new WebClient(token);
-    web.chat.postMessage('testorders', 'Order Received', function (err, res) {
+    web.chat.postMessage('orders', 'Order Received', function (err, res) {
         if (err) {
             console.log('Error:', err);
         } else {
